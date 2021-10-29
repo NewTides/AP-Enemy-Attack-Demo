4af34926547cd0aec5bbad6b681f086cf38bae57
NewTides <jlevy.75583@farnborough.ac.uk>
2021-10-29 13:14:37 +0100

    minor update

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,7 +7,7 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //bullet starting y = 5.28f
     }
 
     // Update is called once per frame
